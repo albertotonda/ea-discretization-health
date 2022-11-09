efb8fa9ad080fec8240439013f1e14d5c702621a
albertotonda <alberto.tonda@gmail.com>
2022-11-10 00:04:37 +0100

    Minor modifications in the script that generates images.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Code and data for the GECCO Paper Mouhrim N. et al. "An Evolutionary Approach to the Discretization of Gene Expression Profiles to Predict the Severity of  COVID-19", 2022
 
 The results for the paper were obtained using the code in src/EAprofileGenerator2Point.py
+
+## TODO
+For plots: https://stackoverflow.com/questions/37759475/scatter-plot-same-point-repeated-several-times-python
--- src/generateFigures.py
+++ src/generateFigures.py
@@ -46,7 +46,44 @@ def main() :
 
     plt.savefig("pareto-fronts.png", dpi=300)
     plt.close(fig)
-    
+
+
+    # another version: size of point is scaled on the number of times it appears
+    # step 1: frequency, how many times does the exact same combination of accuracy and number of profiles appear?
+    frequency_dict = {}
+    for i in range(0, len(accuracies)) :
+        key = (profiles[i], accuracies[i])
+        if key not in frequency_dict :
+            frequency_dict[key] = 1
+        else :
+            frequency_dict[key] += 1
+
+    # find highest frequency
+    max_frequency = max(frequency_dict.values())
+
+    # modify each value in the dictionary to get a size between 1 and 20
+    for key, value in frequency_dict.items() :
+        frequency_dict[key] = int(frequency_dict[key] / float(max_frequency) * 19) + 10
+
+    profiles = [ key[0] for key, value in frequency_dict.items() ]
+    accuracies = [ key[1] for key, value in frequency_dict.items() ]
+    sizes = [ value for key, value in frequency_dict.items() ] 
+    print(sizes)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.scatter(profiles, accuracies, marker='o', color='blue', alpha=0.3, s=sizes, label='Candidate solution')
+    ax.invert_yaxis()
+
+    ax.set_xlabel("Number of different profiles")
+    ax.set_ylabel("Accuracy")
+    ax.set_title("Candidate solutions on the Pareto fronts of 30 experiments")
+    #ax.legend(loc='best')
+    ax.legend(loc='upper right', scatterpoints=3, labelspacing=1)
+
+    plt.savefig("pareto-fronts-different-size.png", dpi=300)
+    plt.close(fig)
 
     return
 
